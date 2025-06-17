e5f312326075fa8fbaee59a4e6e3a374427cd9d2
SantiagoBLP <36992482+SantiagoBLP@users.noreply.github.com>
2025-06-17 18:30:20 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()  # carga .env en desarrollo
-from app import create_app, db
+load_dotenv()
+
+from app import create_app
+
 app = create_app()
 
 if __name__ == '__main__':
+    # Creación de tablas (solo en dev)
     with app.app_context():
+        from app.models import db
         db.create_all()
+    # Ejecutar con Waitress
     from waitress import serve
     serve(app, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
