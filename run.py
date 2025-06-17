e2474430ce434fc0f829a820c9e53d9351480db5
SantiagoBLP <36992482+SantiagoBLP@users.noreply.github.com>
2025-06-17 18:40:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Creación de tablas (solo en dev)
+    # Crear tablas en desarrollo
     with app.app_context():
         from app.models import db
         db.create_all()
-    # Ejecutar con Waitress
+    # Servir con Waitress en local
     from waitress import serve
     serve(app, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
