befe76ff2b3dc5f87e972e5b3d7eefa471c4ffff
SantiagoBLP <36992482+SantiagoBLP@users.noreply.github.com>
2025-06-17 18:40:59 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
+import uuid
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy.dialects.postgresql import UUID
-import uuid
 
 from app import db
 
 class User(db.Model, UserMixin):
     id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    ml_token = db.Column(db.String, nullable=True)
+    ml_access_token = db.Column(db.String, nullable=True)
+    ml_refresh_token = db.Column(db.String, nullable=True)
+
+class StoreStat(db.Model):
+    id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    total_sales = db.Column(db.Integer, default=0)
+    last_updated = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
